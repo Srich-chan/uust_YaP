75fc1655943a770a5d89b6fcfac92b47d64e5fb2
Srich-chan <bed.hippo@gmail.com>
2025-10-29 20:01:43 +0500

    some

--- YaP/6.cpp
+++ YaP/6.cpp
@@ -1,20 +1,27 @@
 #include <cstdio>
 #include <cmath>
 
+void spaces(int n) {
+    for (int i=0; i < n; ++i) printf(" ");
+}
+
+
 int main() {
     int n, curr;
     printf("\nn = ");
     scanf("%d", &n);
 //    int _matrix[n][n];
-    const int s = (int)log10(n) + 1;
-    int spaces;
+    int ms = floor(log10(n)) + 1;
+
+    // char
+
     for (int i=0; i < n; ++i) {
 
         for (int j = 0; j < n; ++j) {
             curr = j - i + 1;
-            if (curr < 0) {curr = 0;}
-            else
-            for (spaces = s - (int)log10(curr) + 1; spaces > 0; --spaces) printf(" ");
+            if (curr <= 0) {curr = 0; spaces(ms);}
+
+            else spaces(ms - floor(log10(curr)));
 
             printf("%d", curr);
 //            _matrix[i][j] = curr;
