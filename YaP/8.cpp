a94b4e381db3dfb6150e546d049d053c0b3b2513
Srich-chan <bed.hippo@gmail.com>
2025-11-12 00:56:31 +0500

    lecture about pointers

--- YaP/8.cpp
+++ YaP/8.cpp
@@ -1,13 +1,36 @@
+// A
 #include <cstdio>
-
-const unsigned MAX_STR_SIZE = 100;
-char str[MAX_STR_SIZE];
+// B
+#include <iostream>
 
 int main () {
-    scanf("%s", &str);
     unsigned c = 0;
-    for (unsigned i=0; str[i] != NULL; ++i)
-        if (str[i] == 'a') if (str[++i] == 'b') if (str[++i] == 'a') ++c;
+    const unsigned MAX_STR_SIZE = 100;
+    // char str[MAX_STR_SIZE]; // A, B1
+    std::basic_string<char> str; // B2
+    // // A
+    // scanf("%s", &str);
+    // for (unsigned i=0; str[i] != NULL; ++i)
+    //     if (str[i] == 'a') if (str[++i] == 'b') if (str[++i] == 'a') ++c;
+
+    // B1
+    char match[] = "aba";
+    std::getline(std::cin, str);
+    // scanf("%s", &str);
+
+    unsigned i = 0, j = 0;
+
+    while (str[i] != NULL) {
+        if (str[i] == match[j]) ++j;
+        else if (j > 0 | str[i] == match[j - 1]) --j;
+        else j = 0;
+
+        if (j == 2) {
+            j = 1;
+            ++c;
+        }
+        ++i;
+    }
 
     printf("\n%d", c);
 }
