b6a7b0ba8e51310093a7c9b6a4bd7182814afe1d
Srich-chan <bed.hippo@gmail.com>
2025-11-27 02:26:08 +0500

    sync commit

--- YaP/10.cpp
+++ YaP/10.cpp
@@ -5,27 +5,13 @@
 const unsigned size = 1024;
 char str[size];
 
-enum state {
-    decrease = -1,
-    increase = 1
-};
-
-char* spiral(char A[][], int n, int k, int x=n-1, int y=n-1, int turn = decrease) {
-    char res[n * n + 1];
-
-
-    if (n == 1) return res;
-    strcat(res, spiral(A, n-1, k, x - turn, y, -turn));
-    return res;
-}
-
 void f () {
     puts("str:"); gets(str);
     unsigned s = strlen(str);
     const int n = int (ceil(sqrt(s)));
 
     char A[n][n+1];
-    char res[n * n + 1];
+    char res[n * n * 2];
     int i=0;
 
     for (; i < s; ++i) {
@@ -35,32 +21,32 @@ void f () {
         A[i / n][i % n] = '#';
     }
 
-    // Diag
-    printf("len = %d, n = %d, i = %d : %s \n", s, n, i, str);
     for (auto& l : A)
-        // printf("len = %llu : %s \n", strlen(l), l);
         printf("%s \n", l);
     puts("");
-    i=0;
-    int x=n-1, y=n-1, k=0, turn = -1;
 
-    while (1) {
-        for (; turn == 1 ? x <= n - k : x >= k; x += turn) {
-            res[i++] = A[y][x];
-            printf("x %d %d %s\n", x, y, turn == 1 ? "+":"-");
-        } x -= turn;
-        y += turn;
-        printf("!%d %d\n", x, y);
-        if (k == n-1) break;
-        for (; turn == 1 ? y <= n - k : y >= k; y += turn) {
-            res[i++] = A[y][x];
-            printf("y %d %d %s\n", x, y, turn == 1 ? "+":"-");
+    i=0; // for res
+    int x=0, y=0, k=0, turn = 1;
+    // | / - - \
+    // | | / \ |
+    // | | X | |
+    // | \ - / |
+    // \ - - - /
+    int max_ = ceil(sqrt(n));
+    while (k < max_){
+        for (; turn == 1 ? y <= n : y >= k ; y += turn) {
+            res[i--] = A[y][x];
         } y -= turn;
+        x += turn;
+        for (; turn == 1 ? y <= n : y >= k ; y += turn) {
+            res[i--] = A[y][x];
+        } x -= turn;
         turn = -turn;
         x += turn;
-        printf("!!%d %d\n", x, y);
+
         ++k;
     }
+    res[0] = A[x][y];
 
     puts(res);
 }
