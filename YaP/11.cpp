c84092068f3ce18b8a9e6889a9521585f12d19cf
Srich-chan <bed.hippo@gmail.com>
2025-12-08 10:13:34 +0500

    sync commit

--- YaP/11.cpp
+++ YaP/11.cpp
@@ -1,6 +1,5 @@
-#include <cstdio>
-#include <cstdlib>
-
+// #include <cstdio>
+// #include <cstdlib>
 // void* ptrs[100];
 // unsigned ptrs_i = 0;
 //
@@ -20,12 +19,39 @@
 // }
 //
 
+
+#include <cstdio>
+#include <cmath>
+#include <clocale>
+
+
 typedef unsigned u16;
 u16 how_many(u16 num) {
-
+    u16 t = 10, c = 1;
+    while (num >= t) {
+        t *= 10; ++c;
+    }
+    return c;
 }
 
+u16 dig_sum(u16 num, u16 c) {
+    u16 t = 1, m = 10, s = 0;
+    for (u16 i=0; i < c; ++i) {
+        s += (num % m) / t;
+        t = m; m *= 10;
+    }
+    return s;
+}
 
 int main() {
-    printf("");
+    setlocale(LC_ALL, "Russian");
+    u16 n;
+    wprintf(L"Натуральное число: "); scanf("%u", &n);
+    if (n == 0) {
+        wprintf(L"Число не натуральное, абортинг...");
+        return -1;
+    }
+    u16 c =  how_many(n);
+    wprintf(L"Результат:\n\tКол-во цифр:%u\n\tСумма цифр:%u", c, dig_sum(n, c));
+
 }
