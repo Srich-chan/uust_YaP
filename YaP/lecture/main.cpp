2c58340bee74432c50463f7b679d751c7c951ae8
Srich-chan <bed.hippo@gmail.com>
2025-12-10 21:18:47 +0500

    deepseek upd1#

--- YaP/lecture/main.cpp
+++ YaP/lecture/main.cpp
@@ -1,48 +1,53 @@
 #include <iostream>
 #include <locale>
+#include <cwchar>
 using namespace std;
 
 struct point {
-    wchar_t * name;
+    wchar_t* name;
     int age;
     point* next;
 };
 
-point* make_point(){
+point* make_point() {
     point* p = new point;
-    setlocale(0, "Russian");
+
     wchar_t buff[64];
     wcout << L"\nВведи имя: ";
     wcin >> buff;
-    p->name = buff;
+
+    p->name = new wchar_t[wcslen(buff) + 1];
+    wcscpy(p->name, buff);
+
     wcout << L"\nВведи возраст: ";
     wcin >> p->age;
-    p->next=nullptr;
+    p->next = nullptr;
+
     return p;
 }
 
 point* make_list(unsigned size) {
-    point* beg=make_point();
+    if (size == 0) return nullptr;
+
+    point* beg = make_point();
     point* temp;
 
-    for (unsigned i=0; i < size; i++) {
+    for (unsigned i = 1; i < size; i++) {
         temp = make_point();
         temp->next = beg;
-
         beg = temp;
     }
     return beg;
 }
 
 void print_point(point* p) {
-    setlocale(0, "Russian");
-    wcout << p->name << '\n';
-    wcout << p->age << '\n';
-    wcout << "- --- - --- - --- -\n";
+    wcout << L"Имя: " << p->name << L'\n';
+    wcout << L"Возраст: " << p->age << L'\n';
+    wcout << L"- --- - --- - --- -\n";
 }
 
 unsigned print_list(point* const beg) {
-    point *p = beg;
+    point* p = beg;
     unsigned count = 0;
     while (p) {
         print_point(p);
@@ -50,12 +55,35 @@ unsigned print_list(point* const beg) {
         ++count;
     }
     return count;
-};
+}
+
+void del_list(point* beg) {
+    point* temp;
+    while (beg) {
+        temp = beg;
+        beg = beg->next;
+        delete[] temp->name;
+        delete temp;
+    }
+}
 
 int main() {
-     setlocale(0, "Russian");
-    unsigned kalvo; cin >> kalvo;
+    setlocale(LC_ALL, "Russian");
+
+    unsigned kalvo;
+    wcout << L"Введите количество элементов: ";
+    wcin >> kalvo;
+
+    if (kalvo == 0) {
+        wcout << L"Список пуст\n";
+        return 0;
+    }
 
     point* list = make_list(kalvo);
+    wcout << L"\nСписок элементов:\n";
     print_list(list);
+
+    del_list(list);
+
+    return 0;
 }
