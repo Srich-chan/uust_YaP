fd71feb3b7b866f35eefa32be64e02ddda950a36
Srich-chan <bed.hippo@gmail.com>
2025-12-22 16:54:26 +0500

    Yap

--- YaP/1st course/9.cpp
+++ YaP/1st course/9.cpp
@@ -2,30 +2,29 @@
 #include <cstring>
 #include <cctype>
 
-#define size 1024
-char str[size], new_str[2 * size];
+#define SIZE 1024
+char str[SIZE], new_str[2 * SIZE];
 constexpr char matchA[] = "<BOLD>";
 constexpr char matchB[] = "<END BOLD>";
 
 unsigned offset = 0;
 unsigned i = 0;
 
-
 int main() {
     puts("str:");
     gets(str);
 
     for (;str[i] != 0; ++i) {
-        int kek = str[i + 1] == '/';
-        if (str[i] == '<' && toupper(str[i + 1 + kek]) == 'B' && str[i + 2 + kek] == '>') {
+        int is_end = str[i + 1] == '/';
+        if (str[i] == '<' && toupper(str[i + 1 + is_end]) == 'B' && str[i + 2 + is_end] == '>') {
             strcat(
                 new_str,
-                !kek ? matchA : matchB
+                !is_end ? matchA : matchB
             );
-            i += 3 + kek;
-            offset += strlen(!kek ? matchA : matchB) - (3 + kek);
+            i += 2 + is_end;
+            offset += strlen(!is_end ? matchA : matchB) - (3 + is_end);
         }
-        new_str[i + offset] = str[i];
+        else new_str[i + offset] = str[i];
     }
     puts(new_str);
 }
