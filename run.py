2f94c8e55d52bd584a62efea6bdf604fd6d21163
Srich-chan <bed.hippo@gmail.com>
2026-03-03 18:08:10 +0500

    run.py

--- run.py
+++ run.py
@@ -9,7 +9,9 @@ p_tasks = join(
     "C:\\", "repos", "user", "CXX_projects", "main_cmake", "YaP", "1st course"
 )
 p_builds = join(p_tasks, "builds")
-
+if task[-4:] == ".cpp": 
+    task = task[:-4]
+print(task)
 variants_to_run = glob(pathname=f"{task}.cpp", root_dir=p_tasks)
 
 task_to_run = variants_to_run[0]
@@ -19,7 +21,7 @@ if variants_to_run.__len__() > 1:
 
 def run(gp_task, gp_build, args=tuple()):
     system(f'g++.exe -std=c++20 "{gp_task}" -o "{gp_build}"')
-    system(f'"{gp_build}"')
+    system(f'"{gp_build}" {" ".join(args)}')
 
 
 run(join(p_tasks, task_to_run), join(p_builds, task_to_run[:-4]), args)
