9112128f694b596e75b5b4ad862edd75e923ccfa
Srich-chan <bed.hippo@gmail.com>
2026-04-08 03:23:51 +0500

    Yap

--- YaP/1st_course/19.cpp
+++ YaP/1st_course/19.cpp
@@ -1,70 +1,97 @@
 #include <iostream>
+#include <fstream>
 #include <locale>
 #include <random>
 #include <set>
 #include <vector>
 #include <windows.h>
-// #include <math.h>
 
-using std::set, std::wcin, std::wcout, std::sqrt, std::cout;
 
-bool is_prime(const int x) {
-    if (x < 2) return false;
-    int cur=2;
-    while (cur < int(sqrt(x)) + 1) {
-        if (x % cur == 0) return false;
-        ++cur;
+// using set, wcin, wcout, sqrt, cout;
+using namespace std;
+
+
+struct diap {
+private:
+    // Кэш
+    vector<int> primes;
+    // Генератор
+    mt19937 rand{1531};
+public:
+    int k; // от
+    int n; // до
+    int min_prime;
+    int max_prime;
+    int m;
+    int p;
+    set<int> set;
+
+    diap (int a, int b, int * _m=nullptr, int * _p=nullptr)
+    : k{a}, n{b} {
+        min_prime = a < 2? 2 : a;
+        max_prime = b;
+        primes.push_back(a);
+        while (!is_prime(min_prime)) ++min_prime;
+        while (!is_prime(max_prime)) --max_prime;
+        while (primes.back() < max_prime) {
+            primes.push_back(
+                get_prime_to_right(primes.back())
+            );
+        }
+        m = !_m? primes[rand() % primes.size()] : *_m;
+        p = !_p? primes[rand() % primes.size()] : *_p;
+
+        for (int i=a; i <= b; ++i) {
+            if (i % m == 0 ^ i % p == 0)
+                set.insert(i);
+        }
     }
-    return true;
-}
 
-int get_prime(int n) {
-    static std::vector primes{2, 3, 5, 7, 11};
-    if (n < 0) throw std::domain_error{"Tupo"};
-    if (n <= primes.size()) return primes[n];
-    for (int i=primes.size(); primes[i] <= n; ++i) {
-        if (is_prime(i+2)) primes.push_back(i);
+    static bool is_prime(const int x) {
+        if (x < 2) return false;
+        int cur = 2;
+        while (x >= cur*cur) {
+            if (x % cur == 0) return false;
+            ++cur;
+        }
+        return true;
     }
-    return primes[n];
-}
 
+    int get_prime_to_left(int normal) {
+        int i=normal-1;
+        while (!is_prime(i)) {
+            if (i <= min_prime) return min_prime;
+            --i;
+        }
+        return i;
+    }
+
+    int get_prime_to_right(int normal) {
+        int i=normal+1;
+        while (!is_prime(i)) {
+            if (i >= max_prime) return max_prime;
+            ++i;
+        }
+        return i;
+    }
+};
+
+#include "paths.h"
 
 int main () {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     setlocale(0, "Russian");
 
-    std::mt19937 rand(0);
+    ifstream tests{p_PROJECT "19 tests.txt"};
+    while (!tests.eof()) {
+        int k, n, w=0, p=0;
+        tests >> k >> n >> w >> p;
+        wcout << L"\n Введены: " << k << L" и " << n << L" и " << w << L" и " << p << "\n";
 
-    int k, n;
-    wcout << L"\nВведите границы диапазона от k до n: ";
-    wcin >> k >> n;
-    wcout << L"\n Введены: " << k << L" и " << n << "\n";
-    set<int> res;
-    get_prime(n);
-    // Любые простые числа до n
-    const int m = get_prime(rand() + 1);
-    const int p = get_prime(rand() + 1);
-
-    wcout <<  L"\nБыли выбраны:\n\tm = " << m <<
-        L"\n\tp = " << p << '\n';
-
-    for (int i=k; i <= n; ++i) {
-        if (i % m == 0 ^ i % p == 0) {
-            res.insert(i);
+        diap diap(k, n, w?&w:nullptr , p?&p:nullptr);
+        for (auto& el : diap.set) {
+            wcout << el << " ";
         }
     }
-
-    wcout << L"Получено мн-во:\n{";
-
-    size_t c = 1;
-
-    for (auto& el : res) {
-        cout << el << (c != res.size() ? ", ":"");
-        if (c % 10 == 0) cout << '\n';
-        ++c;
-    }
-
-    wcout << "}\n";
-
 }
