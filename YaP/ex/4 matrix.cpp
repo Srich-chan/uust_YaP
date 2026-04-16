6fbc14716894df1d5dd080dc26d882e695e7204b
Srich-chan <bed.hippo@gmail.com>
2026-04-16 20:55:02 +0500

    Yap

--- YaP/ex/4 matrix.cpp
+++ YaP/ex/4 matrix.cpp
@@ -10,7 +10,7 @@ class row : public vector<double> {
     using vector::vector;
 
 public:
-    string to_str(int presicion=4) {
+    string str(int presicion=2) {
         ostringstream oss;
         auto& vec = *this;
         oss << fixed << setprecision(presicion); // Не ясная чёрная магия C++
@@ -25,25 +25,19 @@ public:
     }
 };
 
-
-
 constexpr double DEFAULT_VALUE = 67;
 
-
-
-class Matr : public vector<row> {
+class Matr : vector<row> {
     using vector::vector;
-
     size_t m = 4;
     size_t n = 4;
-
 
 public:
     Matr(double val=DEFAULT_VALUE, size_t M=4, size_t N=4) {
         if (M < 1 || N < 1) throw domain_error{"Unreal condition!"};
         m = M; n = N;
 
-        this->resize(m);
+        resize(m);
         for (row& row : *this) {
             row.resize(n);
             for (double& el : row) {
@@ -52,25 +46,31 @@ public:
         }
     }
 
-    Matr(const vector<row>& ref) {
-        *this = ref;
-        m = ref.size();
-        if (!ref.empty())
-            n = ref[0].size();
-        else
-            n = 0;
-    }
+    void reform(size_t M, size_t N) {
+        if (M < 1 || N < 1) throw domain_error{"Unreal condition!"};
+        m = M; n = N;
 
-    string to_str(bool mult_lines=1, bool postline=1){
+        resize(m);
+        for (row& row : *this) {
+            row.resize(n);
+        }
+    }
+    
+    string str(bool mult_lines=1, bool postline=1){
         ostringstream oss;
         Matr& self = *this;
 
+        // for (auto& row : self) {
+            // oss << row.to_str() << (mult_lines? ",\n " : ", ");
+        // }
 
-        oss << "[";
-        for (auto& row : self) {
-            oss << row.to_str() << mult_lines? ",\n " : ", ";
+        oss << "[" << self[0].str();
+        for (int i=1; i < m; ++i) {
+            row& row = self[i];
+            oss << (mult_lines? ",\n " : ", ") << row.str();
         }
         oss << "]";
+
         if (postline)
             oss << "\n";
 
@@ -79,33 +79,9 @@ public:
 };
 
 
-class Matrix {
-    int m, n;
-protected:
-    vector<row> self;
-public:
-    Matrix(int M, int N, double val=DEFAULT_VALUE) {
-        if (M < 1 || N < 1) throw domain_error{"Unreal condition!"};
-        m = M; n = N;
-        const row base = row(N, val);
-        self.resize(M);
-        for (row& el : self) {
-            el = base;
-        }
-    }
-
-    explicit Matrix(vector<row>& mtrx) {
-        self = mtrx;
-        m = static_cast<int>(mtrx.size());
-        n = static_cast<int>(mtrx[0].size());
-    }
-
-
-
-};
-
 int main() {
-    cout << Matr{}.to_str() << "\n";
+    cout << Matr{}.str() << "\n";
+    cout << Matr{1.1}.str();
 
-    cout << Matr{1.1}.to_str();
+    cout << Matr{67, 5, 10}.str();
 }
