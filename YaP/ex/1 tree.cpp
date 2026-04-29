31b977818c062c5b1550d04e6a1e0e7cdb6705ee
Srich-chan <bed.hippo@gmail.com>
2026-04-30 03:44:47 +0500

    Yap

--- YaP/ex/1 tree.cpp
+++ YaP/ex/1 tree.cpp
@@ -4,97 +4,103 @@
 
 using namespace std;
 
-class branch {
-    branch* add_sibling(int value=228) {
-        branch* head = this;
-        while (head->next_sibling != nullptr) {
-            head = head->next_sibling;
-        }
-        branch*& sib = head->next_sibling;
-        sib = new branch{head->parent};
-        sib->prev_sibling = head;
-        sib->ass = value;
-
-        return sib;
-    }
-
-    size_t count_with_siblings() {
-        size_t c = 1;
-        if (child != nullptr) c += child->count_with_siblings();
-        if (next_sibling != nullptr) c += next_sibling->count_with_siblings();
-        return c;
-    }
-
-    size_t _count_leaves() {
-        size_t c = child == nullptr;
-        if (child != nullptr) c += child->_count_leaves();
-        if (next_sibling != nullptr) c += next_sibling->_count_leaves();
-        return c;
-    }
-public:
-
-    branch* add_child(int value=10) {
-        if (child == nullptr) {
-            child = new branch{this};
-            child->ass = value;
-            return child;
-        }
-        return child->add_sibling(value);
-    }
-
-    size_t tree_size() {
-        if (child) return 1 + child->count_with_siblings();
-        return 1;
-    }
-
-    size_t count_leaves() {
-        if (child) return child->_count_leaves();
-        return 1;
-    }
-
-    size_t count_childs() {
-        if (!child) return 0;
-
-        branch* head = child;
-        size_t c = 1;
-        while (head->next_sibling != nullptr) {
-            head = head->next_sibling;
-            ++c;
-        }
-        return c;
-    }
-
-    branch* operator[](size_t ind) {
-        if (!child) throw std::domain_error{"puk"};
-        if (ind >= count_childs()) throw std::domain_error{"srenk"};
-        branch* head=child;
-        for (size_t i=1; i < ind; ++i) {
-            head = head->next_sibling;
-        }
-        return head;
-    }
-
-    branch* parent=nullptr;
-    branch* child=nullptr;
-    branch* prev_sibling=nullptr;
-    branch* next_sibling=nullptr;
-    int ass=67;
-
-    ~branch() {
-        delete child;
-        delete next_sibling;
-    }
-};
-
 
 class tree{
 public:
+
+    class branch {
+        branch* add_sibling(int value=228) {
+            branch* head = this;
+            while (head->next_sibling != nullptr) {
+                head = head->next_sibling;
+            }
+            branch*& sib = head->next_sibling;
+            sib = new branch{head->parent};
+            sib->prev_sibling = head;
+            sib->_value = value;
+
+            return sib;
+        }
+
+        size_t count_with_siblings() {
+            size_t c = 1;
+            if (child != nullptr) c += child->count_with_siblings();
+            if (next_sibling != nullptr) c += next_sibling->count_with_siblings();
+            return c;
+        }
+
+        size_t count_leaves_rec() {
+            /*
+             Иди нахуй Луценко
+             */
+            size_t rec_count = !child;
+            if (child)
+                rec_count += child->count_leaves_rec();
+            if (next_sibling)
+                rec_count += next_sibling->count_leaves_rec();
+            return rec_count;
+        }
+    public:
+
+        branch* add_child(int value=10) {
+            if (child == nullptr) {
+                child = new branch{this};
+                child->_value = value;
+                return child;
+            }
+            return child->add_sibling(value);
+        }
+
+        size_t tree_size() {
+            if (child) return 1 + child->count_with_siblings();
+            return 1;
+        }
+
+        size_t count_leaves() {
+            if (child) return child->count_leaves_rec();
+            return 1;
+        }
+
+        size_t count_childs() {
+            if (!child) return 0;
+
+            branch* head = child;
+            size_t c = 1;
+            while (head->next_sibling != nullptr) {
+                head = head->next_sibling;
+                ++c;
+            }
+            return c;
+        }
+
+        branch* operator[](size_t ind) {
+            if (!child) throw std::domain_error{"puk"};
+            if (ind >= count_childs()) throw std::domain_error{"srenk"};
+            branch* head=child;
+            for (size_t i=1; i < ind; ++i) {
+                head = head->next_sibling;
+            }
+            return head;
+        }
+
+        branch* parent=nullptr;
+        branch* child=nullptr;
+        branch* prev_sibling=nullptr;
+        branch* next_sibling=nullptr;
+
+        int _value=67;
+
+        ~branch() {
+            delete child;
+            delete next_sibling;
+        }
+    };
+
     branch* root;
     // Пустое древо
     tree() {
         root = new branch();
     }
-
     // Дерево созданное броском кубика, где есть хотя бы 10 веток-узлов
     tree(std::mt19937 rand) {
         enum actions {
@@ -140,10 +146,11 @@ public:
 };
 
 
+
 int main() {
     std::mt19937 gen(123);
-    tree puk(gen);
+    tree VELIKOE_DREVO(gen);
 
-    cout << "Leaves: " << puk.root->count_leaves();
-    cout << "\nTotal Size: " << puk.root->tree_size();
+    cout << "Leaves: " << VELIKOE_DREVO.root->count_leaves();
+    cout << "\nTotal Size: " << VELIKOE_DREVO.root->tree_size();
 }
